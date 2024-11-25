dad9c6e46503ae8dcc17a797166fb541ae1b43d5
Euclides Marques <euclidesmarques270@gmail.com>
2024-11-24 21:57:25 -0300

    Implementando a logica de xequemate

--- xadrez_console/Program.cs
+++ xadrez_console/Program.cs
@@ -44,11 +44,15 @@ namespace xadrez_console
                         Console.ReadLine();
                     }
                 }
+                Console.Clear();
+                Tela.imprimirPartida(partida);
             }
             catch (TabuleiroException e)
             {
                 Console.WriteLine(e.Message);
             }
+
+            Console.ReadLine();
         }
     }
 }
